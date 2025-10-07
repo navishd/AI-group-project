0cda38bc3ed8e3812423ed6eb99422ec2020e818
amantha2002 <amanthasandunaa@gmail.com>
2025-10-08 00:49:59 +0530

    make the error

--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 import { clerkMiddleware, requireAuth } from '@clerk/express'
-
-
+import aiRouter from './routes/aiRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
-
-app.get('/', (req,res)=>res.send('server is Live!'))
-
+app.get('/',(req, res)=>res.send('server is live'))
 app.use(requireAuth())
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT,()=>{
-  console.log('server is running on port',PORT);
+app.use('/api/ai',aiRouter)
+app.use('/api/user',userRouter)
+const PORT = process.env.PORT ||3000;
+app.listen(PORT , ()=>{
+    console.log('server is running on port',PORT);
 })
